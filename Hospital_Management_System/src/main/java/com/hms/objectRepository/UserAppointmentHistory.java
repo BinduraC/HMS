965e24c37ca2078f4d3c7b0cd9e0ba1e5c7354bf
Indrajitm96 <moharanaindrajit@gmail.com>
2022-08-04 18:32:02 +0530

    patients documents

---
+++ Hospital_Management_System/src/main/java/com/hms/objectRepository/UserAppointmentHistory.java
@@ -0,0 +1,30 @@
+package com.hms.objectRepository;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+/**
+ * 
+ * @author INDRAJIT
+ *
+ */
+public class UserAppointmentHistory {
+
+	//initialization
+	public UserAppointmentHistory(WebDriver driver)
+	{
+		PageFactory.initElements(driver, this);
+	}
+	
+	//declaration
+	@FindBy(xpath = "//td[2]") private WebElement doctorsName;
+	
+	//utilization
+	public WebElement getDoctorsName() {
+		return doctorsName;
+	}
+	
+	
+}
