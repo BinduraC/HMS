aa138bcd5388bd7b3027473828aaa7e72c2dd297
hemanth <dinuhemanth@gmail.com>
2022-08-05 15:00:39 +0530

    added new doctorclass with author name dinuh

---
+++ Hospital_Management_System/src/main/java/com/hms/objectRepository/DoctorChangePasswordPage.java
@@ -0,0 +1,46 @@
+package com.hms.objectRepository;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+/**
+ * 
+ * @author dinuh
+ *
+ */
+public class DoctorChangePasswordPage 
+{
+	//declaration
+	@FindBy(xpath="//input[@name='cpass']") private WebElement currentPasswordEdt;
+	
+	@FindBy(xpath="//input[@name='npass']") private WebElement newPasswordEdt;
+	
+	@FindBy(xpath="//input[@name='cfpass']") private WebElement confirmPasswordEdt;
+	
+	@FindBy(xpath="//button[@name='submit']")private WebElement submitBtn;
+	
+	//initialization
+	public DoctorChangePasswordPage(WebDriver driver) 
+	{
+		PageFactory.initElements(driver, this);
+	}
+	
+	//utilization
+	public WebElement getCurrentPasswordEdt() {
+		return currentPasswordEdt;
+	}
+
+	public WebElement getNewPasswordEdt() {
+		return newPasswordEdt;
+	}
+
+	public WebElement getConfirmPasswordEdt() {
+		return confirmPasswordEdt;
+	}
+
+	public WebElement getSubmitBtn() {
+		return submitBtn;
+	}
+	
+}
